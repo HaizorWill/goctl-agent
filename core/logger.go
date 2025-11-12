53f1da6ffc66b09611409ceffecbd7ddde68533c
HaizorWill <thelines6@gmail.com>
2025-11-12 22:54:41 +0300

    Add logger

---
+++ core/logger.go
@@ -0,0 +1,29 @@
+package core
+
+import (
+	"log/slog"
+	"os"
+)
+
+var Logger *slog.Logger
+
+func LogInit() {
+	if Logger == nil {
+		LogHandler := slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{
+			Level: slog.LevelInfo,
+		})
+		Logger = slog.New(LogHandler)
+	}
+}
+
+func LogInfo(msg string, args ...any) {
+	Logger.Info(msg, args...)
+}
+
+func LogWarn(msg string, args ...any) {
+	Logger.Warn(msg, args...)
+}
+
+func LogError(msg string, args ...any) {
+	Logger.Error(msg, args...)
+}
