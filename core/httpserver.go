d821702aa91271661fd1fab2e92d397e7e4038d0
HaizorWill <thelines6@gmail.com>
2025-11-13 14:08:52 +0300

    Add simple GET and POST functions for convienence

--- core/httpserver.go
+++ core/httpserver.go
@@ -6,11 +6,20 @@ import (
 
 type HttpServer struct {
 	mux *http.ServeMux
-	Handler
+	*Handler
 }
 
 func HttpInit(h Handler) *HttpServer {
 	mux := http.NewServeMux()
-	mux.HandleFunc("/", h.Services)
-	return &HttpServer{mux: mux}
+	return &HttpServer{mux: mux, Handler: &h}
+}
+
+func (s *HttpServer) GET(relativePath string, handler HandlerFunc) {
+	path := http.MethodGet + " " + relativePath
+	s.mux.HandleFunc(path, handler)
+}
+
+func (s *HttpServer) POST(relativePath string, handler HandlerFunc) {
+	path := http.MethodPost + " " + relativePath
+	s.mux.HandleFunc(path, handler)
 }
