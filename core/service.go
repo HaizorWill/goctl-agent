a284e3a2721766eabdc8782f0fd8866d0dae6d27
HaizorWill <thelines6@gmail.com>
2025-11-18 00:10:16 +0300

    Bruh

--- core/service.go
+++ core/service.go
@@ -6,18 +6,68 @@ package core
 
 import (
 	"fmt"
+	"path/filepath"
+	"strings"
+	"time"
 
-	"github.com/coreos/go-systemd/v22/dbus"
+	dbus "github.com/coreos/go-systemd/v22/dbus"
 )
 
 type Service struct {
 	*Application
-	ch chan string
+	ch        chan string
+	unitFiles []dbus.UnitFile
 }
 
 func NewService(a *Application) *Service {
 	ch := make(chan string)
 	return &Service{Application: a, ch: ch}
+}
+
+// Burn this whole fucking code alive and never ever touch it again
+func (s *Service) Initialize() {
+	go func() {
+		for {
+			unitFiles, err := s.Connection.ListUnitFilesContext(s.Context)
+			if err == nil {
+				s.mu.Lock()
+				s.unitFiles = unitFiles
+				s.mu.Unlock()
+				time.Sleep(30 * time.Second)
+			}
+			time.Sleep(30 * time.Second)
+		}
+	}()
+}
+
+func (s *Service) ListUnits() (units []dbus.UnitStatus, err error) {
+	loadedUnits, err := s.Connection.ListUnitsContext(s.Context)
+	if err != nil {
+		return
+	}
+	unitMap := make(map[string]dbus.UnitStatus)
+	for _, unit := range loadedUnits {
+		unitMap[unit.Name] = unit
+	}
+	for _, unitFile := range s.unitFiles {
+		unitName := filepath.Base(unitFile.Path)
+		if _, exists := unitMap[unitName]; !exists {
+			unitMap[unitName] = dbus.UnitStatus{
+				Name:        unitName,
+				LoadState:   "unloaded",
+				ActiveState: "inactive",
+				SubState:    "dead",
+			}
+		}
+	}
+
+	for _, unit := range unitMap {
+		if strings.HasSuffix(unit.Name, ".service") {
+			units = append(units, unit)
+		}
+	}
+
+	return
 }
 
 func (s *Service) ListAllUnits() (units []dbus.UnitStatus, err error) {
