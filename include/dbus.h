485f118630c980b3dc897ab1ae70424d4cdd6562
HaizorWill <thelines6@gmail.com>
2025-11-25 01:32:23 +0300

    Add header for dbus.c

--- include/dbus.h
+++ include/dbus.h
@@ -1,3 +1,5 @@
 typedef struct systemd_bus systemd_bus;
 
 struct systemd_bus *new_systemd_bus(void);
+int systemd_bus_call(struct systemd_bus *dbus);
+char *systemd_bus_message_read(struct systemd_bus *dbus);
