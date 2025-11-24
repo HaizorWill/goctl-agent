c7d91c191e4014505c707d049c2259098e9d8c30
HaizorWill <thelines6@gmail.com>
2025-11-25 01:32:59 +0300

    Add testing wiring

--- src/main.c
+++ src/main.c
@@ -1,7 +1,13 @@
 #include <stdbool.h>
 
+#include "../include/dbus.h"
 #include <systemd/sd-bus.h>
 
 bool loadConfig(void) { return false; }
 
-int main() { return 0; }
+int main() {
+  struct systemd_bus *bus = new_systemd_bus();
+  systemd_bus_call(bus);
+  char *ans = systemd_bus_message_read(bus);
+  return 0;
+}
