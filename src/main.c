f7503a0f0b451159ef6223d41ec0d8c150bf540d
HaizorWill <thelines6@gmail.com>
2025-11-25 21:18:41 +0300

    Call conn_close

--- src/main.c
+++ src/main.c
@@ -1,6 +1,7 @@
 #include <stdbool.h>
 
 #include "dbus.h"
+#include <stdint.h>
 #include <stdio.h>
 #include <systemd/sd-bus.h>
 #include <unistd.h>
@@ -10,8 +11,10 @@ bool loadConfig(void) { return false; }
 int main() {
   struct systemd_bus *bus = new_systemd_bus();
   systemd_bus_set_dest(bus);
-  systemd_bus_call(bus, "GetUnitByPID", "u", (unsigned)getpid());
+  unsigned pid = 1;
+  systemd_bus_call(bus, "GetUnitByPID", "u", pid);
   char *ans = systemd_bus_message_read(bus, "o");
   printf("Here: %s", ans);
+  systemd_bus_conn_close(bus);
   return 0;
 }
