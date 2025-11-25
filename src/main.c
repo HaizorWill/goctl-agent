84841abb2e9e2da493e4d4462418a55fd0392cf2
HaizorWill <thelines6@gmail.com>
2025-11-25 17:49:50 +0300

    Change testing wiring

--- src/main.c
+++ src/main.c
@@ -1,13 +1,17 @@
 #include <stdbool.h>
 
-#include "../include/dbus.h"
+#include "dbus.h"
+#include <stdio.h>
 #include <systemd/sd-bus.h>
+#include <unistd.h>
 
 bool loadConfig(void) { return false; }
 
 int main() {
   struct systemd_bus *bus = new_systemd_bus();
-  systemd_bus_call(bus);
-  char *ans = systemd_bus_message_read(bus);
+  systemd_bus_set_dest(bus);
+  systemd_bus_call(bus, "GetUnitByPID", "u", (unsigned)getpid());
+  char *ans = systemd_bus_message_read(bus, "o");
+  printf("Here: %s", ans);
   return 0;
 }
