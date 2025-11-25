7c0dc7da95934fc824f365502503cedbad2fbef5
HaizorWill <thelines6@gmail.com>
2025-11-25 17:49:11 +0300

    Add set_dest function, change arguments

--- src/dbus.c
+++ src/dbus.c
@@ -8,6 +8,9 @@ struct systemd_bus {
   sd_bus *bus;
   sd_bus_error *err;
   sd_bus_message *mes;
+  const char *dest;
+  const char *path;
+  const char *iface;
 };
 
 struct systemd_bus *new_systemd_bus(void) {
@@ -18,25 +21,32 @@ struct systemd_bus *new_systemd_bus(void) {
   return dbus;
 }
 
+void systemd_bus_set_dest(struct systemd_bus *dbus) {
+  dbus->dest = "org.freedesktop.systemd1";
+  dbus->path = "/org/freedesktop/systemd1";
+  dbus->iface = "org.freedesktop.systemd1.Manager";
+}
 // Not too sure what to do with this to be frank :/
 // Needs to be flexible, so that we are able to call whatever
 // Yet i am not too sure how to go about it
 // Might pass parameters directly into it, though it is dumb, since it does
 // exactly what it does under the hood
-int systemd_bus_call(struct systemd_bus *dbus) {
-  // int err = sd_bus_call_method();
-  int err = sd_bus_call_method(
-      dbus->bus, "org.freedesktop.systemd1", "/org/freedesktop/systemd1",
-      "org.freedesktop.systemd1.Manager", "GetUnitByPID", dbus->err, &dbus->mes,
-      "u", (unsigned)getpid());
+int systemd_bus_call(struct systemd_bus *dbus, const char *member,
+                     const char *types, ...) {
+  va_list args;
+  va_start(args, types);
+  int err = sd_bus_call_method(dbus->bus, dbus->dest, dbus->path, dbus->iface,
+                               member, dbus->err, &dbus->mes, types, args);
+  va_end(args);
+
   if (err < 0)
     return 1;
   return 0;
 }
 
-char *systemd_bus_message_read(struct systemd_bus *dbus) {
+char *systemd_bus_message_read(struct systemd_bus *dbus, const char *types) {
   char *ans;
-  int err = sd_bus_message_read(dbus->mes, "", &ans);
+  int err = sd_bus_message_read(dbus->mes, types, &ans);
   if (err < 0)
     return NULL;
   return ans;
