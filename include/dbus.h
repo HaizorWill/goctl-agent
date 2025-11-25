ed48a10b7ccf07f5b2a604361948a8d929018b63
HaizorWill <thelines6@gmail.com>
2025-11-25 17:48:14 +0300

    Add function definition, change arguments

--- include/dbus.h
+++ include/dbus.h
@@ -1,5 +1,7 @@
 typedef struct systemd_bus systemd_bus;
 
 struct systemd_bus *new_systemd_bus(void);
-int systemd_bus_call(struct systemd_bus *dbus);
-char *systemd_bus_message_read(struct systemd_bus *dbus);
+void systemd_bus_set_dest(struct systemd_bus *dbus);
+int systemd_bus_call(struct systemd_bus *dbus, const char *member,
+                     const char *types, ...);
+char *systemd_bus_message_read(struct systemd_bus *dbus, const char *types);
