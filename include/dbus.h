eb52021c0d3b073fdc5fa95956c8d091dacf9626
HaizorWill <thelines6@gmail.com>
2025-11-25 20:46:16 +0300

    Add conn_close definition

--- include/dbus.h
+++ include/dbus.h
@@ -1,6 +1,7 @@
 typedef struct systemd_bus systemd_bus;
 
 struct systemd_bus *new_systemd_bus(void);
+void systemd_bus_conn_close(struct systemd_bus *dbus);
 void systemd_bus_set_dest(struct systemd_bus *dbus);
 int systemd_bus_call(struct systemd_bus *dbus, const char *member,
                      const char *types, ...);
